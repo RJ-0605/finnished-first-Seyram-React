dfcff65b819da8e1c3438b5723f5e21de502cc93
rj-0605 <rojedkay@gmail.com>
2020-12-02 12:02:40 +0000

    Seyram to get link to hosted

---
+++ src/components/OnHeroContents.js
@@ -0,0 +1,260 @@
+import React from 'react';
+import './OnheroContents.css';
+
+export default function OnHeroContents() {
+
+	return (
+		<>
+			<div className="maindiv-contents">
+
+				<h4 className="personal-title"> Personal information </h4>
+
+				<div className="persondiv">
+					<div className="persondivdiv">
+						<h5>Mobile Number</h5>
+						<p>002245</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Email</h5>
+						<p>rooney@email.com</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Nationality</h5>
+						<p>gambian</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Occupation</h5>
+						<p>er</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Date of Birth</h5>
+						<p>2020-05-09</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Gender</h5>
+						<p>male</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="maindiv-contents">
+				<h4 className="personal-title"> Medical information </h4>
+
+				<table>
+					<tr className="head">
+						<th>SUFFER FROM</th>
+						<th>STATUS</th>
+						<th>YEAR OF DIAGNOSES</th>
+						<th>TYPE</th>
+						<th>MEDICATION</th>
+					</tr>
+					<tr>
+						<td> <span>diabetes</span></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><span>hypertension</span></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><span>thyroid</span></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+
+				<div className="persondiv">
+					<div className="persondivdiv">
+						<h5>Food Allergies</h5>
+						<p>None</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Any other health problems?</h5>
+						<p>None</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>When was your last blood test?</h5>
+						<p>gambian</p>
+					</div>
+				</div>
+
+				<table>
+					<tr className="head">
+						<th>SUFFER FROM</th>
+						<th>RANGE</th>
+						<th>MEDICATION</th>
+					</tr>
+					<tr>
+						<td> <span>cholesterol</span></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><span>Triglycerides</span></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+
+				<div className="persondiv">
+					<div className="persondivdiv">
+						<h5>Iron deficiency</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Vitamin D deficiency</h5>
+						<p>No</p>
+					</div>
+				</div>
+
+				<table>
+					<tr className="head">
+						<th>SUFFER FROM</th>
+						<th>SINCE WHEN</th>
+						<th>TREATMENT</th>
+					</tr>
+					<tr>
+						<td> <span>constipation</span></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><span>bloating</span></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+
+			</div>
+
+			<div className="maindiv-contents">
+				<h2 className="personal-title"> Lifestyle information </h2>
+				<div className="persondiv">
+					<div className="persondivdiv">
+						<h5>Previous Diet Plans</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Skip Meals</h5>
+						<p>yes <br /> yes</p>
+						{/* <p>yes</p> */}
+					</div>
+					<div className="persondivdiv">
+						<h5>Regular Breakfast</h5>
+						<p>No</p>
+					</div>
+
+					<div className="persondivdiv">
+						<h5>Snack</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Food Cravings</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Exercise</h5>
+						<p>No</p>
+					</div>
+
+					<div className="persondivdiv">
+						<h5>Smoking</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Coffee</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Alcohol intake</h5>
+						<p>No</p>
+					</div>
+
+					<div className="persondivdiv">
+						<h5>Litres of water per day</h5>
+						<p>20</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Breakfast</h5>
+						<p>sdasdf</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Lunch</h5>
+						<p>fsdf</p>
+					</div>
+
+					<div className="persondivdiv">
+						<h5>Dinner</h5>
+						<p>adf</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Snack</h5>
+						<p>No</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Cook own meals</h5>
+						<p>No</p>
+					</div>
+
+				</div>
+			</div>
+
+			<div className="maindiv-contents">
+				<h2 className="personal-title"> Delivery information </h2>
+				<div className="persondiv">
+					<div className="persondivdiv">
+						<h5>City</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Area</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Street Address</h5>
+						<p>-, -</p>
+					</div>
+
+					<div className="persondivdiv">
+						<h5>House Number</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Landmark</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Longitude</h5>
+						<p>-</p>
+					</div>
+
+					<div className="persondivdiv">
+						<h5>Latitude</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Preferred Delivery Time</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Pickup Name</h5>
+						<p>-</p>
+					</div>
+					<div className="persondivdiv">
+						<h5>Pickup Phone</h5>
+						<p>-</p>
+					</div>
+				</div>
+
+			</div>
+		</>
+	)
+}
